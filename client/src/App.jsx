e90e4af9f14c3e26fced34fc03fd45e251216534
mermer-mac <mincoln419@naver.com>
2026-08-24 21:05:18 +0900

    Fix misplaced closing bracket in tab conditional rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -207,10 +207,10 @@ export default function App() {
         <div className="flex-1 min-h-0 overflow-y-auto">
           <WebhookSettings />
         </div>
+      )}
 
       {tab === 'longgoals' && (
         <LongGoals />
-
       )}
     </div>
   );
